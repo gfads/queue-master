5255d659499c58cb56c4ac9824c20f8efaa3977e
Adalberto Junior <adalrsjr1@gmail.com>
2017-10-05 11:42:24 -0700

    spawner connecting to local Docker

--- src/main/java/works/weave/socks/queuemaster/DockerSpawner.java
+++ src/main/java/works/weave/socks/queuemaster/DockerSpawner.java
@@ -30,12 +30,12 @@ public class DockerSpawner {
 		if (dc == null) {
 			DefaultDockerClientConfig.Builder config = DefaultDockerClientConfig.createDefaultConfigBuilder();
 //
-            config.withDockerTlsVerify(true)
-	            .withDockerCertPath("/usr/src/app/certs")
+//            config.withDockerTlsVerify(true)
+//	            .withDockerCertPath("/usr/src/app/certs")
 //    		      .withDockerCertPath("C:/Users/adalr/Development/java/maverick/kay-certs/.certs")
-	              .withDockerHost("tcp://kay.cs.ubc.ca:2376")
-	              .withApiVersion("1.23")
-	              .build();
+//	              .withDockerHost("tcp://kay.cs.ubc.ca:2376")
+//	              .withApiVersion("1.23")
+//	              .build();
                
             dc = DockerClientBuilder.getInstance(config).build();
             dc.pullImageCmd(imageName).withTag(imageVersion).exec(new PullImageResultCallback()).awaitSuccess();
@@ -44,20 +44,23 @@ public class DockerSpawner {
 			dockerPool = Executors.newFixedThreadPool(poolSize);
 		}
 	}
+		
+	public static void main(String[] args) {
+		DockerSpawner ds = new DockerSpawner();
+		ds.init();
+		ds.spawn();
+	}
 	
-	
-//	public static void main(String[] args) {
-//		DockerSpawner ds = new DockerSpawner();
-//		ds.init();
-//		ds.spawn();
-//	}
+	private String getWeaveNetworkId() {
+		return dc.listNetworksCmd().withNameFilter(networkId).exec().get(0).getName();
+	}
 
 	public void spawn() {
 		dockerPool.execute(new Runnable() {
 		    public void run() {
 				logger.info("Spawning new container");
 				try {
-					CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withNetworkMode(networkId).withCmd("ping", "rabbitmq").exec();
+					CreateContainerResponse container = dc.createContainerCmd(imageName + ":" + imageVersion).withNetworkMode(getWeaveNetworkId()).withCmd("ping", "rabbitmq").exec();
 					String containerId = container.getId();
 					dc.startContainerCmd(containerId).exec();
 					logger.info("Spawned container with id: " + container.getId() + " on network: " + networkId);
@@ -77,4 +80,5 @@ public class DockerSpawner {
 		    }
 		});
 	}
+	
 }
